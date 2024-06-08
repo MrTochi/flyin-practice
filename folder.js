83fd264e324372f115238f42d73e0032ac6bb74e
MrTochi <okonkwotochuks@gmail.com>
2024-06-08 20:05:52 +0100

    flyin

---
+++ folder.js
@@ -0,0 +1,36 @@
+let myicon=document.getElementById(`icon`)
+let flyin=document.getElementById(`flyin`)
+let main=document.getElementById(`main`)
+
+
+
+
+
+
+function openFlyin(){
+ 
+}
+myicon.addEventListener(`click`,function(event){
+    event.stopPropagation()
+    if(flyin.classList.contains(`edit-delete-box`)){
+        flyin.classList.remove(`edit-delete-box`)
+        flyin.classList.add(`edit-delete-box-new`)
+       }
+       else if(flyin.classList.contains(`edit-delete-box-new`)){
+        flyin.classList.remove(`edit-delete-box-new`)
+        flyin.classList.add(`edit-delete-box`)
+       }
+})
+
+function close(){
+    if(flyin.classList.contains(`edit-delete-box-new`)){
+        flyin.classList.remove(`edit-delete-box-new`)
+        flyin.classList.add(`edit-delete-box`)
+       }
+}
+
+main.addEventListener(`click`,close)
+
+
+
+
